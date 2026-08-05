98d4e0631bbfe70934035a0462c02db2470ce037
cristiannav <cristiannav.arg@gmail.com>
2026-08-05 17:53:38 -0300

    Se agregan cambios

--- frontend/src/sections/About.tsx
+++ frontend/src/sections/About.tsx
@@ -32,7 +32,7 @@ export function About() {
           <dl className="space-y-4 text-sm">
             <div>
               <dt className="text-slate-400">Experiencia</dt>
-              <dd className="text-white">+6 años como Full Stack Developer</dd>
+              <dd className="text-white">+6 años como desarrollador</dd>
             </div>
             <div>
               <dt className="text-slate-400">En TI</dt>
